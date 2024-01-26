f0290c41fcbb2e445cabafa7c22a195702df04ad
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-01-26 16:28:12 +0100

    modifica non allert se non si inserisce un numero

--- js/codice.js
+++ js/codice.js
@@ -7,6 +7,15 @@ let km = Number( prompt ("inserire i chilometri da percorrere") );
 // abbiamo inserito number per definire sin da subito il valore inserito come numero e non come stringa
 let Età = Number( prompt ("inserire la propria età") ); 
 
+if(isNaN(Età))
+{
+    alert("hai inserito il numsero");
+}
+else
+{
+    alert("non è un numero ma ti svegli");
+}
+
 // variabile usata per riproporre a schermo i km inseriti
 document.getElementById("Età").innerHTML = `Questa è l'Età inserita ${Età}anni`;
 
@@ -23,21 +32,29 @@ console.log(Età);
 const sconto_over = Number (40);
 const sconto_under = Number (20);
 
-
+// definizioni variabili e calcolo costo tratta
 let Costo_biglietto ;
 let costo_tratta = (km * 0.21);
 
+// primo snodo della nostra condizione
 if(Età >= 65 ) 
 {
     Costo_biglietto = Number ((costo_tratta - ((costo_tratta * sconto_over)/ 100 ) ));
+    // parseFloat viene usato in questo caso insieme ad to.fixed(2) perchè fixed restituisce una stringa con 2 valori decimali e parse lo traforma in numero nuovamente
+    Costo_biglietto = parseFloat(Costo_biglietto.toFixed(2));
     document.getElementById("Costo_biglietto").innerHTML = `Questo è quanto spendi ${Costo_biglietto}`;
- 
+    
 }
+// secondo snodo della nostra condizione
 else if(Età <= 17) 
 {
     Costo_biglietto = Number ((costo_tratta - ((costo_tratta * sconto_under)/ 100 ) ));
+    // parseFloat viene usato in questo caso insieme ad to.fixed(2) perchè fixed restituisce una stringa con 2 valori decimali e parse lo traforma in numero nuovamente
+    Costo_biglietto = parseFloat(Costo_biglietto.toFixed(2));
     document.getElementById("Costo_biglietto").innerHTML = `Questo è quanto spendi ${Costo_biglietto}`;
+    
 }
+// terzo snodo della nostra condizione
 else
 {
     Costo_biglietto = Number (costo_tratta) ;
