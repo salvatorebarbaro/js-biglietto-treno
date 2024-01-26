f8150fad3e9beb25b6e9af68aa82ee4f62251b94
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-01-26 17:36:13 +0100

    fine

--- js/codice.js
+++ js/codice.js
@@ -1,36 +1,52 @@
 
 // comando usato per interfacciarsi con l'utente per richiesta km e dichiarazione variabile
 // abbiamo inserito number per definire sin da subito il valore inserito come numero e non come stringa
-let km = Number( prompt ("inserire i chilometri da percorrere") );
+let km = ( prompt ("inserire i chilometri da percorrere") );
+
+// isnan variabile o coamndo usato per definire se sia un numero o no
+if( (isNaN(km))){
+    alert("non è un numero");
+    let errore_km;
+    errore_km = km;
+    document.getElementById("errore_km").innerHTML = `hai sbagliato inserendo euesto valore ${errore_km}`;
+}else if( km <= 0){
+    alert("sei sicuro di aver messo il numero giusta?");
+    let errore_km;
+    errore_km = km;
+    document.getElementById("errore_km").innerHTML = `hai sbagliato inserendo euesto valore ${errore_km}`;
+}else{ 
+    alert("hai inserito il numero");
+    // variabile usata per riproporre a schermo i km inseriti
+    document.getElementById("km").innerHTML = `Questi sono i km ${km}inseriti`;
+}
 
 // comando usato per interfacciarsi con l'utente per richiesta km e dichiarazione variabile
 // abbiamo inserito number per definire sin da subito il valore inserito come numero e non come stringa
-let Età = Number( prompt ("inserire la propria età") ); 
+let Eta = ( prompt ("inserire la propria età") ); 
 
 // isnan variabile o coamndo usato per definire se sia un numero o no
-if( ((isNaN(Età)) && (Età <= 0)) || (Età > 99) )
-{
-    alert("non è un numero ma ti svegli ricarica la pagina");
-}
-
-else
-{
-    
+if( (isNaN(Eta))){
+    alert("non è un numero");
+    let errore_Eta;
+    errore_Eta = Eta;
+    document.getElementById("errore_Eta").innerHTML = `hai sbagliato inserendo euesto valore ${errore_Eta}`;
+}else if( Eta <= 0 || Eta > 99 ){
+    alert("sei sicuro di aver messo l'età giusta?");
+    let errore_Eta;
+    errore_Eta = Eta;
+    document.getElementById("errore_Eta").innerHTML = `hai sbagliato inserendo euesto valore ${errore_Eta}`;
+}else{ 
     alert("hai inserito il numero");
+    // variabile usata per riproporre a schermo i km inseriti
+    document.getElementById("Età").innerHTML = `Questa è l'Età inserita ${Eta}anni`;
 }
 
-
-// variabile usata per riproporre a schermo i km inseriti
-document.getElementById("Età").innerHTML = `Questa è l'Età inserita ${Età}anni`;
-
-// variabile usata per riproporre a schermo i km inseriti
-document.getElementById("km").innerHTML = `Questi sono i km inseriti ${km}`;
 
 // comando usato per visualizzare risultato
 console.log(km);
 
 // comando usato per visualizzare risultato
-console.log(Età);
+console.log(Eta);
 
 // dichiarazione degli sconti 
 const sconto_over = Number (40);
@@ -41,7 +57,7 @@ let Costo_biglietto ;
 let costo_tratta = (km * 0.21);
 
 // primo snodo della nostra condizione
-if(Età >= 65 ) 
+if(Eta >= 65 ) 
 {
     Costo_biglietto = Number ((costo_tratta - ((costo_tratta * sconto_over)/ 100 ) ));
     // parseFloat viene usato in questo caso insieme ad to.fixed(2) perchè fixed restituisce una stringa con 2 valori decimali e parse lo traforma in numero nuovamente
@@ -50,7 +66,7 @@ if(Età >= 65 )
     
 }
 // secondo snodo della nostra condizione
-else if(Età <= 17) 
+else if(Eta <= 17) 
 {
     Costo_biglietto = Number ((costo_tratta - ((costo_tratta * sconto_under)/ 100 ) ));
     // parseFloat viene usato in questo caso insieme ad to.fixed(2) perchè fixed restituisce una stringa con 2 valori decimali e parse lo traforma in numero nuovamente
