81c3d85f44395a5de8aa107c9ef3b56fe74d51a9
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-01-26 16:31:34 +0100

    cambiamento dettagli e commenti

--- js/codice.js
+++ js/codice.js
@@ -7,11 +7,12 @@ let km = Number( prompt ("inserire i chilometri da percorrere") );
 // abbiamo inserito number per definire sin da subito il valore inserito come numero e non come stringa
 let Età = Number( prompt ("inserire la propria età") ); 
 
-// controllo con relativo messaggio di allert se si inserisce un numero 
+// isnan variabile o coamndo usato per definire se sia un numero o no
 if(isNaN(Età))
 {
     alert("hai inserito il numsero");
 }
+
 else
 {
     alert("non è un numero ma ti svegli");
